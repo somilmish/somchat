a72fc2c31fdcf2a1f5f0d2f93cd543e315e13838
somilmish <somilmish@gmail.com>
2018-07-05 18:55:07 +0530

    Added code in show.html.erb to allow the user to post messages Added subscription model for rooms (users can subscribe to rooms) Added length validation for messages

--- app/controllers/chat_room_controller.rb
+++ app/controllers/chat_room_controller.rb
@@ -23,6 +23,7 @@ class ChatRoomController < ApplicationController
 
   def show
     @chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
+    @message = Message.new
   end
 
   private
