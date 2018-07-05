9ff5ef8fa094b6c758084e1012cbf514d9765c37
somilmish <somilmish@gmail.com>
2018-07-05 18:37:02 +0530

    Configuring action cable using redis

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'chat_room#home'
   resources :chat_rooms, only: [:new, :create, :show, :index]
+  mount ActionCable.server => '/cable'
 end
